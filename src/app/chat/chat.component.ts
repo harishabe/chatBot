c8df2eb90cc9681c9f0a5166d1813b02ef3305f8
harisha <harish90142@gmail.com>
2018-04-23 23:45:16 +0530

    chat bot

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Message } from '../models/message'
+import { Component, OnInit, AfterViewInit, ViewChild, ViewChildren, QueryList, ElementRef  } from '@angular/core';
+//import { Message } from '../models/message'
 import { ChatService } from '../service/chat.service';
 
 @Component({
@@ -8,27 +8,47 @@ import { ChatService } from '../service/chat.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  public message: Message;
-  public messages: Message[];
+  messages = [];
+  userMsg = { "sent": '' };
+  errorMsg = '';
+
+  // Code for Scrolling
+  @ViewChild('chatlist', { read: ElementRef }) chatList: ElementRef;
+  @ViewChildren(ChatComponent, { read: ElementRef }) chatItems: QueryList<ChatComponent>;
+
   constructor(private chatService: ChatService) {
-    this.message = new Message('', 'assets/images/user.png', new Date());
-    this.messages = [
-      new Message('Welcome to chatbot universe', 'assets/images/bot.png', new Date())
-    ];
   }
 
   ngOnInit() {
-    this.sendMessage();
+    //this.sendMessage();
+  }
+
+  ngAfterViewInit() {
+    this.chatItems.changes.subscribe(elements => {
+      this.scrollToBottom();
+    });
   }
 
   sendMessage() {
-    this.message["timestamp"] = new Date();
-    this.messages.push(this.message);
-    this.chatService.getResponse(this.message["content"]).subscribe(res => {
-      this.messages.push(
-        new Message(res.result.fulfillment.speech, 'assets/images/bot.png', res.timestamp)
-      );
-    }, err => console.log(err))
-    this.message = new Message('', 'assets/images/user.png', new Date);
+    if (this.userMsg.sent !== '') {
+      this.errorMsg = '';
+      this.messages.push({ "sent": this.userMsg.sent });
+      this.chatService.getResponse(this.userMsg["sent"]).subscribe(res => {
+        this.messages.push({ "replies": res.result.fulfillment.speech });
+        console.log('this.messages', this.messages);
+      }, err => console.log(err));
+    } else {
+      this.errorMsg = 'Please Enter Text';
+    }
   }
+
+  private scrollToBottom(): void {
+    try {
+      this.chatList.nativeElement.scrollTop = this.chatList.nativeElement.scrollHeight;
+    }
+    catch (err) {
+      console.log('Could not find the "chatList" element.');
+    }
+  }
+
 }
